9d65a7454cc6660aecf83464372740449145449a
Roy Mdr <go_rojo@hotmail.com>
2022-08-29 19:48:45 -0500

    Basic implementation of own Sortable.js mod
    Own git fork: https://github.com/roy-mdr/Sortable

--- client-spa/gtd-app-spa/src/components/Inbox.svelte
+++ client-spa/gtd-app-spa/src/components/Inbox.svelte
@@ -8,36 +8,85 @@
 
     let foo, bar;
 
-    let myList = [{name:'First item', id:1}, {name:'Second item', id:2}, {name:'Third item', id:3}]
+    let myList1 = [{name:'1', id:1}, {name:'2', id:2}, {name:'3', id:3}]
+    let myList2 = [{name:'A', id:4}, {name:'B', id:5}, {name:'C', id:6}]
 
     function setUpSortable() {
         Sortable.create(foo, {
             group: {
                 name: 'foo',
-                // put: true,
+                put: true,
                 // pull: false,
             },
-            animation: 200
+            revertDOM: true,
+            animation: 200,
+            delay: 200,
+            delayOnTouchOnly: true,
+            onAdd: (e) => {
+                // drag from one list and drop into another
+
+                console.log(e.toSortable)
+                console.log(e.fromSortable)
+                if (e.fromSortable.options.group.name == 'bar') {
+                    myList1.splice(e.newIndex, 0, myList2[e.oldIndex]);
+                    if (e.pullMode !== 'clone') {
+                        // delete
+                        myList2.splice(e.oldIndex, 1);
+                    }
+                    myList1 = myList1;
+                    myList2 = myList2;
+                }
+
+            },
+            onSort: (e) => {
+                // drag & drop within the same list
+                console.log("sort tabla 1 :)")
+                handleContainerChange(myList1, e);
+                myList1 = myList1;
+            }
         });
 
         Sortable.create(bar, {
             group: {
                 name: 'bar',
-                // put: true,
-                // pull: 'clone',
+                put: true,
+                pull: 'clone',
+                // pull: false,
             },
+            revertDOM: true,
             animation: 200,
+            delay: 200,
+            delayOnTouchOnly: true,
             invertSwap: true,
-            onSort: (e) => handleContainerChange(e)
+            swapThreshold: 0.25,
+            onAdd: (e) => {
+                // drag from one list and drop into another
+
+                if (e.fromSortable.options.group.name == 'foo') {
+                    myList2.splice(e.newIndex, 0, myList1[e.oldIndex]);
+                    if (e.pullMode !== 'clone') {
+                        // delete
+                        myList1.splice(e.oldIndex, 1);
+                    }
+                    myList2 = myList2;
+                    myList1 = myList1;
+                }
+
+            },
+            onSort: (e) => {
+                // drag & drop within the same list
+                console.log("sort tabla 2 :)")
+                handleContainerChange(myList2, e);
+                myList2 = myList2;
+            }
         });
     };
 
-    function handleContainerChange(ev) {
+    function handleContainerChange(list, ev) {
         console.log("Container changed!", ev);
-        const oldLi = myList[ev.oldIndex];		
-        myList.splice(ev.oldIndex, 1);
-        myList.splice(ev.newIndex, 0, oldLi);
-        myList = myList;
+        const oldLi = list[ev.oldIndex];
+        list.splice(ev.oldIndex, 1);
+        list.splice(ev.newIndex, 0, oldLi);
     }
 </script>
 
@@ -46,16 +95,18 @@
 <div>
     <h3>Inbox</h3>
     <div class="container" bind:this={foo}>
-        <p class="draggable" draggable="true">1</p>
-        <p class="draggable" draggable="true">2</p>
+        {#each myList1 as listItem, i (listItem.id)}
+        <p class="draggable">{i}. {listItem.name}</p>
+        {/each}
     </div>
     <div class="container" bind:this={bar}>
-        {#each myList as listItem, i (listItem.id)}
-        <p class="draggable" draggable="true">{i}. {listItem.name}</p>
+        {#each myList2 as listItem, i (listItem.id)}
+        <p class="draggable">{i}. {listItem.name}</p>
         {/each}
     </div>
 </div>
-<pre><code>{JSON.stringify(myList, null, 2)}</code></pre>
+<pre><code>{JSON.stringify(myList1, null, 2)}</code></pre>
+<pre><code>{JSON.stringify(myList2, null, 2)}</code></pre>
 
 
 
